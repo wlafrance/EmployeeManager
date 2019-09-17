2021e8dacba8862b0b540c4fc387e83511dfe987
Warren LaFrance <lafrance.warren@jcdev.org>
2019-09-17 07:45:19 -0500

    Added login and register pages with shallow scripts

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,6 +3,17 @@
     <div class="nav-wrapper green">
       <div class="container">
         <router-link to="/" class="brand-logo">Employee Manager</router-link>
+        <ul class="right">
+          <li>
+            <router-link to="/">Dashboard</router-link>
+          </li>
+          <li>
+            <router-link to="/login">Login</router-link>
+          </li>
+          <li>
+            <router-link to="/register">Register</router-link>
+          </li>
+        </ul>
       </div>
     </div>
   </nav>
--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,44 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Dashboard from "@/components/Dashboard";
-import NewEmployee from "@/components/NewEmployee";
-import EditEmployee from "@/components/EditEmployee";
-import ViewEmployee from "@/components/ViewEmployee";
+import Vue from 'vue';
+import Router from 'vue-router';
+import Login from '@/components/Login';
+import Register from '@/components/Register';
+import Dashboard from '@/components/Dashboard';
+import NewEmployee from '@/components/NewEmployee';
+import EditEmployee from '@/components/EditEmployee';
+import ViewEmployee from '@/components/ViewEmployee';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: "/",
-      name: "Dashbaord",
+      path: '/',
+      name: 'Dashbaord',
       component: Dashboard
     },
     {
-      path: "/new",
-      name: "new-employee",
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
+    },
+    {
+      path: '/new',
+      name: 'new-employee',
       component: NewEmployee
     },
     {
-      path: "/edit/:employee_id",
-      name: "edit-employee",
+      path: '/edit/:employee_id',
+      name: 'edit-employee',
       component: EditEmployee
     },
     {
-      path: "/:employee_id",
-      name: "view-employee",
+      path: '/:employee_id',
+      name: 'view-employee',
       component: ViewEmployee
     }
   ]
